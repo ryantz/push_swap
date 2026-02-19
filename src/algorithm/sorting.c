dbcecd9d9887d3efe788b7bbb2a4662a464a761c
Ryan Zhiyang Tan <ryatan@c2r5s13.42singapore.sg>
2026-02-19 15:14:42 +0800

    commented out empty fn

--- src/algorithm/sorting.c
+++ src/algorithm/sorting.c
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:27:25 by ryatan            #+#    #+#             */
-/*   Updated: 2026/02/19 15:11:30 by ryatan           ###   ########.fr       */
+/*   Updated: 2026/02/19 15:14:27 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,14 @@ int	ft_sort_two(t_stack **stack_a)
 	return (0);
 }
 
-int	ft_sort_three(t_stack **stack_a)
-{
-}
-
-int	ft_sort_four(t_stack **stack_a)
-{
-}
-
-int	ft_sort_five(t_stack **stack_a)
-{
-}
+//int	ft_sort_three(t_stack **stack_a)
+//{
+//}
+//
+//int	ft_sort_four(t_stack **stack_a)
+//{
+//}
+//
+//int	ft_sort_five(t_stack **stack_a)
+//{
+//}
