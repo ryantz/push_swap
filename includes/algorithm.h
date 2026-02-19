84739d50484ad6581f3ea6afe1678b75fbe35f28
Ryan Zhiyang Tan <ryatan@c2r5s13.42singapore.sg>
2026-02-19 15:17:31 +0800

    included move_operations library in algorithms.h

--- includes/algorithm.h
+++ includes/algorithm.h
@@ -6,7 +6,7 @@
 /*   By: ryatan <ryatan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/19 14:25:49 by ryatan            #+#    #+#             */
-/*   Updated: 2026/02/19 15:02:21 by ryatan           ###   ########.fr       */
+/*   Updated: 2026/02/19 15:16:40 by ryatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define ALGORITHM_H
 
 # include "push_swap.h"
+# include "move_operations.h"
 
 int	ft_sort_two(t_stack **stack_a);
 
